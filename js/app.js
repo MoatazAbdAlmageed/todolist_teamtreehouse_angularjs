bf7175dbe7e6b10208c8366fb5f37ed222a99ca6
Moataz <m.mohammedy@tie.com.eg>
2016-12-04 10:38:33 +0200

    03_03-Using ngrepeat to inject HTML for every data element

--- js/app.js
+++ js/app.js
@@ -6,6 +6,61 @@
 var myApp = angular.module('todoListApp',[]);
 
 
-myApp.controller('GreetingController', ['$scope', function($scope) {
-    $scope.task = 'Hola!';
+myApp.controller('mainCtrl', ['$scope', function($scope) {
+$scope.todos = [
+    {"name" :"angularjs coues"},
+    {"name" : "rails coues"},
+    {"name" : "tie website"},
+    {"name" : "lacreationarts website"},
+    {"name" : "Kimai web application"},
+
+];
+    $scope.todo ={};
+    // $scope.editlabel ="Edit";
+    $scope.todo.name = 'a simple todo!';
+    //
+    // if ($scope.editing =true){
+    // $scope.editlabel ="Save";
+    // }
+    // else    {
+    //
+    // $scope.editlabel ="Edit";
+    // }
+
+
+    $scope.saveTodo = function () {
+    } ;
+
+    $scope.editTodo = function () {
+        //
+        // if ($scope.todo.editing  == true) {
+        // $scope.todo.editing = false;
+        //
+        // }
+
+        console.log('edittodo');
+    };
+
+    $scope.deleteTodo = function () {
+        console.log('deletetodo');
+    };
+
 }]);
+
+myApp.controller('newCtrl', ['$scope', function($scope) {
+    $scope.whoIAm = function () {
+        console.log('Iam a newCtrl');
+    }
+}]);
+
+
+
+// myApp.directive("w3TestDirective", function() {
+//     return {
+//         template : "Made by a directive!",
+//         restrict :"C",
+//         restrict :"A",
+//         restrict :"M",
+//         restrict :"E",
+//     };
+// });
